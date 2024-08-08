a23962d6a6ea858f94e776b37ef24865c1ebeac9
ahmed dacharoui <dachraouia193@gmail.com>
2024-08-08 18:36:23 +0100

    first one

---
+++ src/main/java/org/newapp/service_booking_sys/ServiceBookingSysApplication.java
@@ -0,0 +1,13 @@
+package org.newapp.service_booking_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBookingSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceBookingSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/newapp/service_booking_sys/ServiceBookingSysApplicationTests.java
@@ -0,0 +1,13 @@
+package org.newapp.service_booking_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBookingSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
